2a86ddb3f8b138da5e230bcc93e11a31a1041c20
Bommu Nikhil <nikhilbommu14399@gmail.com>
2020-05-09 13:24:11 +0530

    maximum added

---
+++ Leetcode/LeetCode Problems/Maximum69Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        temp = num
+        add = 0
+        mul = 1
+        while num > 0:
+            rem = num % 10
+            if rem == 6:
+                add = 3 * mul
+            mul = mul * 10
+            num = num // 10
+        return temp + add
